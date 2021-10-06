e2cc1bf48efc53e765463a58bb16b53d962dbbf7
Tyler Norman <tnorman600@gmail.com>
2021-10-06 15:50:28 -0600

    started work on search history. Not persistent, but a users city will be converted to title case

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,9 @@ $("#city-input").keypress(function(e){
 });
 
 $("#search-button").click(function(){
-    cityName = $("#city-input").val().trim();
+    cityName = $("#city-input").val().trim().toLowerCase().replace(/\b[a-z]/g, function(txtVal) {
+        return txtVal.toUpperCase();
+    });    
     apiKey = "c5f2523844ee1178a0e74a7c87427cf4";
     apiURL = "https://api.openweathermap.org/data/2.5/weather?q="
     + cityName + "&units=imperial&appid=" + apiKey;
@@ -15,13 +17,20 @@ $("#search-button").click(function(){
         if (response.ok) {
             response.json().then(function(data) {
                 console.log(data);
+                logSearchHistory();
             });
 
             $("#city-input").focus(function(){
                 $(this).val("");
             });
         } else {
-            alert("There was an error. Please refresh the page and try again.");
+            alert("You may have entered an invalid city name or weather services may be down. Please try again.");
         }
     });
 });
+
+function logSearchHistory() {
+    searchHistoryEl = '<div class="forecast col text-center bg-primary text-white mx-0 my-3 rounded">' + cityName + '</div>';
+    $("#search-history").append(searchHistoryEl);
+    
+}
