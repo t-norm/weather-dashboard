7f9e2844ac9a1917764fab7c4c7faab9dcf07115
Tyler Norman <tnorman600@gmail.com>
2021-10-06 14:29:49 -0600

    user can search a city by hitting enter and searcfh bar will be reset after a successaful search and the search bar loses and regains focus

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,24 @@
+$("#city-input").keypress(function(e){
+    if (e.which == 13){
+        $("#search-button").click();
+    }
+});
+
 $("#search-button").click(function(){
     cityName = $("#city-input").val().trim();
     apiKey = "c5f2523844ee1178a0e74a7c87427cf4";
     apiURL = "https://api.openweathermap.org/data/2.5/weather?q="
-    + cityName + "&appid=" + apiKey;
+    + cityName + "&units=imperial&appid=" + apiKey;
     console.log(cityName)
 
     fetch(apiURL).then(function(response){
         if (response.ok) {
             response.json().then(function(data) {
                 console.log(data);
+            });
+
+            $("#city-input").focus(function(){
+                $(this).val("");
             });
         } else {
             alert("There was an error. Please refresh the page and try again.");
