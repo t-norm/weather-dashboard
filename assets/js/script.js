6a1eee559b94a6b5fc1890e493dc4e7b41283d03
Tyler Norman <tnorman600@gmail.com>
2021-10-07 16:17:34 -0600

    city name and current weather icon now appear upon city search

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,13 +11,22 @@ $("#search-button").click(function(){
     apiKey = "c5f2523844ee1178a0e74a7c87427cf4";
     apiURL = "https://api.openweathermap.org/data/2.5/weather?q="
     + cityName + "&units=imperial&appid=" + apiKey;
-    console.log(cityName)
 
     fetch(apiURL).then(function(response){
         if (response.ok) {
             response.json().then(function(data) {
                 console.log(data);
                 logSearchHistory();
+                
+                const date = new Date(data.dt*1000);
+                const month = date.getMonth() + 1;
+                const day = date.getDate();
+                const year = date.getFullYear();
+                let currentWeatherIcon = data.weather[0].icon;
+
+                $("#city-name").text(data.name + " | " + month + "/" + day + "/" + year);
+                $("#current-weather-icon").attr("src", "https://openweathermap.org/img/wn/" + currentWeatherIcon + "@2x.png");
+                $("#current-weather-icon").attr("alt", data.weather[0].description);
             });
         } else {
             alert("You may have entered an invalid city name or weather services may be down. Please try again.");
@@ -28,9 +37,7 @@ $("#search-button").click(function(){
 });
 
 $(document).on('click',"#history-delete",function() {
-    console.log($(this).parents("history-container"));
     $(this).parents("#history-container").remove();
-    
 });
 
 function logSearchHistory() {
@@ -39,5 +46,4 @@ function logSearchHistory() {
     <div class="col-1 d-flex justify-content-end pt-1">
     <i class="fas fa-minus-circle" id="history-delete"></i></div></div>`
     $("#search-history").prepend(searchHistoryEl);
-    
 }
